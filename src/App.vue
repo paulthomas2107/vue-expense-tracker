4b307d18cda2c08fbc27987f1a7324718975384e
Paul Thomas <paulthomas2107@gmail.com>
2023-11-08 18:40:36 +0000

    xxx

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <Header></Header>
   <div class="container">
     <Balance />
+    <IncomeExpenses />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
 export default {
   components: {
     Header,
     Balance,
+    IncomeExpenses,
   },
 };
 </script>
