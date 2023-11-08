c2e3de6fb6ceefa6fa548ae501c7441ebc7bc5ef
Paul Thomas <paulthomas2107@gmail.com>
2023-11-08 18:28:26 +0000

    Add balance

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,17 @@
-<template>My App</template>
+<template>
+  <Header></Header>
+  <div class="container">
+    <Balance />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+export default {
+  components: {
+    Header,
+    Balance,
+  },
+};
+</script>
